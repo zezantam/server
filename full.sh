3880e632ab7fb047cc76576becf94ffbe14f8ff2
sidazhang <sidazhang89@gmail.com>
2013-12-16 13:16:59 -0800

    Update full.sh

--- full.sh
+++ full.sh
@@ -1,6 +1,12 @@
-curl -L https://raw.github.com/sidazhang/server/master/modules/core.sh | sh
-curl -L https://raw.github.com/sidazhang/server/master/modules/dev_environment.sh | sh
-curl -L https://raw.github.com/sidazhang/server/master/modules/postgres.sh | sh
+echo "----------Core----------"
+curl -O https://raw.github.com/sidazhang/server/master/modules/core.sh && chmod a+x core.sh && && rm -f core.sh
+echo "----------dev env----------"
+curl -O https://raw.github.com/sidazhang/server/master/modules/dev_environment.sh && chmod a+x dev_environment.sh && && rm -f dev_environment.sh
+echo "----------postgres----------"
+curl -O https://raw.github.com/sidazhang/server/master/modules/postgres.sh && chmod a+x postgres.sh && && rm -f postgres.sh
+echo "----------ruby----------"
 curl -O https://raw.github.com/sidazhang/server/master/modules/ruby.sh && chmod a+x ruby.sh && ./ruby.sh && rm -f ruby.sh
-sudo curl -L https://raw.github.com/sidazhang/server/master/modules/ruby_gems.sh | sh
-curl -L https://raw.github.com/sidazhang/server/master/modules/bitcoin.sh | sh
+echo "----------ruby gems----------"
+curl -O https://raw.github.com/sidazhang/server/master/modules/ruby_gems.sh && chmod a+x ruby_gems.sh && && rm -f ruby_gems.sh
+echo "----------bitcoin----------"
+curl -O https://raw.github.com/sidazhang/server/master/modules/bitcoin.sh && chmod a+x bitcoin.sh && && rm -f bitcoin.sh
