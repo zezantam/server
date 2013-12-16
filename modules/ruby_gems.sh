99fb80412292061d03823af32967eff52b7e77b3
sidazhang <sidazhang89@gmail.com>
2013-12-16 13:10:19 -0800

    Update ruby_gems.sh

--- modules/ruby_gems.sh
+++ modules/ruby_gems.sh
@@ -1,10 +1,10 @@
-echo -e "\033[31m Default no documentations in gems"
+echo "\033[31m Default no documentations in gems"
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
-echo -e "\033[31m Installing RubyGems"
+echo "\033[31m Installing RubyGems"
 sudo apt-get install -y rubygems
 
-echo -e "\033[31m Installing basic gems"
+echo "\033[31m Installing basic gems"
 sudo gem install bundler
 sudo gem install sinatra
 sudo gem install bitcoin-ruby
