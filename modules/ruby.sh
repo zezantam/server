ac4b50fdf557d22b3ac3a653075de643a6465fdc
sidazhang <sidazhang89@gmail.com>
2013-12-16 12:01:05 -0800

    Update ruby.sh

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -13,14 +13,6 @@ echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 echo "reload path"
 exec $SHELL
 
-echo "Build tools"
-sudo apt-get update
-sudo apt-get install -y build-essential
-sudo apt-get install -y git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev nohup
-
-echo "Reload Path"
-exec $SHELL
-
 echo "Installing Ruby 2.0.0"
 rbenv install 2.0.0-p353
 rbenv global 2.0.0-p353
