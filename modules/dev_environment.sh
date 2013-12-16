cec22f401123a020857d852ef96b3860555845a2
sidazhang <sidazhang89@gmail.com>
2013-12-16 13:12:50 -0800

    Update dev_environment.sh

--- modules/dev_environment.sh
+++ modules/dev_environment.sh
@@ -1,19 +1,19 @@
-echo -e "\033[31m Installing ctags"
+echo "Installing ctags"
 sudo apt-get -y install exuberant-ctags
 
-echo -e "\033[31m Installing ultimate vimrc"
+echo "Installing ultimate vimrc"
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo -e "\033[31m Installing zsh"
+echo "Installing zsh"
 sudo apt-get -y install zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 # TODO: chsh doesn't work on aws
 sudo chsh -s /bin/zsh
 
-echo -e "\033[31m Adding custom vim configs"
+echo "Adding custom vim configs"
 cd ~/.vim_runtime/ 
 curl -O https://gist.github.com/sidazhang/7896694/raw/my_configs.vim
 
-echo -e "\033[31m Getting Golang Syntax Highligting"
+echo "Getting Golang Syntax Highligting"
 git clone https://github.com/jnwhiteh/vim-golang.git ~/.vim_runtime/sources_non_forked/vim-golang
