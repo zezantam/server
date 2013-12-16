6d67044d0d2186b70eb34572dd3ae95154d74ddf
sidazhang <sidazhang89@gmail.com>
2013-12-16 12:14:40 -0800

    Update dev_environment.sh

--- modules/dev_environment.sh
+++ modules/dev_environment.sh
@@ -8,15 +8,12 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "Installing zsh"
 sudo apt-get -y install zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-# chsh doesn't work on aws
+# TODO: chsh doesn't work on aws
 sudo chsh -s /bin/zsh
-echo "zsh" >> ~/.bash_profile
 
 echo "Happy Coding!"
-zsh
 cd ~/.vim_runtime/ 
 curl -O https://gist.github.com/sidazhang/7896694/raw/my_configs.vim
 
 echo "Getting Golang Syntax Highligting"
 git clone https://github.com/jnwhiteh/vim-golang.git ~/.vim_runtime/sources_non_forked/vim-golang
-zsh
