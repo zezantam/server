a435a9618a1268a77e138b50e1041b07643475c1
sidazhang <sidazhang89@gmail.com>
2013-12-16 12:03:55 -0800

    Update ruby.sh

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -10,8 +10,6 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 
-source ~/.bash_profile
-
 echo "Installing Ruby 2.0.0"
 rbenv install 2.0.0-p353
 rbenv global 2.0.0-p353
