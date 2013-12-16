0409a1b3982aee1ce745e707fdd356993db80369
sidazhang <sidazhang89@gmail.com>
2013-12-16 11:40:02 -0800

    Update full.sh

--- full.sh
+++ full.sh
@@ -1,1 +1,6 @@
 curl -L https://raw.github.com/sidazhang/server/master/modules/core.sh | sh
+curl -L https://raw.github.com/sidazhang/server/master/modules/dev_environment.sh | sh
+curl -L https://github.com/sidazhang/server/blob/master/modules/postgres.sh | sh
+curl -L https://raw.github.com/sidazhang/server/master/modules/ruby.sh | sh
+curl -L https://raw.github.com/sidazhang/server/master/modules/ruby_gems.sh | sh
+curl -L https://raw.github.com/sidazhang/server/master/modules/bitcoin.sh | sh
