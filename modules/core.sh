d27c9537b749c5fe652d74ea62a68ca3623cad18
sidazhang <sidazhang89@gmail.com>
2013-12-16 20:34:45 -0800

    Update core.sh

--- modules/core.sh
+++ modules/core.sh
@@ -1,4 +1,5 @@
 echo "Installing Core Tools"
 sudo apt-get update
 sudo apt-get install -yf build-essential
-sudo apt-get install -yf git git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
+sudo apt-get install -yf git git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev sqlite3 libxml2-dev libxslt1-dev
+sudo apt-get install -yf libsqlite3-dev
