8acd3e6facaed4f2be858a0bfe185a0d2f16f417
sidazhang <sidazhang89@gmail.com>
2013-12-18 09:48:58 -0800

    Update core.sh

--- modules/core.sh
+++ modules/core.sh
@@ -4,3 +4,4 @@ sudo apt-get install -yf build-essential
 sudo apt-get install -yf git git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev sqlite3 libxml2-dev libxslt1-dev
 sudo apt-get install -yf libsqlite3-dev
 sudo apt-get install -yf nodejs
+sudo apt-get install -yf python-dev
