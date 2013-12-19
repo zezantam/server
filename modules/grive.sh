eeed7b99f8cac7f00659c1c17d7190f80811d7cf
sidazhang <sidazhang89@gmail.com>
2013-12-19 10:17:31 -0800

    Update grive.sh

--- modules/grive.sh
+++ modules/grive.sh
@@ -1,3 +1,4 @@
+echo "Note: This requires Ruby"
 echo "Installing depedencies"
 sudo apt-get install -yf cmake build-essential libgcrypt11-dev libjson0-dev libcurl4-openssl-dev libexpat1-dev libboost-filesystem-dev libboost-program-options-dev libboost-all-dev
 
