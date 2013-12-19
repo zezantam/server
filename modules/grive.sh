4dcaf4addb7ef6298a583841538aab8ed65bd532
sidazhang <sidazhang89@gmail.com>
2013-12-19 12:25:06 -0800

    Update grive.sh

--- modules/grive.sh
+++ modules/grive.sh
@@ -3,21 +3,8 @@ echo "Note: yajl requires Ruby"
 echo "Installing depedencies"
 sudo apt-get install -yf cmake build-essential libgcrypt11-dev libjson0-dev libcurl4-openssl-dev libexpat1-dev libboost-filesystem-dev libboost-program-options-dev libboost-all-dev binutils
 
-echo "Installing yajl"
-git clone git://github.com/lloyd/yajl yajl
-cd yajl
-ruby configure
-cmake .
-make
-sudo make install
-cd ~
-
-echo "installing grive"
-git clone git://github.com/Grive/grive.git
-cd ./grive/grive
-cmake .
-make
-sudo make install
-
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install grive
 
 
