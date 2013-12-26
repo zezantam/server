1dd64d968c134a63e2c750b2b1b0eec3a5b6cde4
sidazhang <sidazhang89@gmail.com>
2013-12-26 15:33:29 -0800

    Update go.sh

--- modules/go.sh
+++ modules/go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo rm -rf /usr/local/go
-wget https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz -O /tmp/go.tar.gz
+wget https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz -O /tmp/go.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 rm -rf /tmp/go.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" | sudo cat >> ~/.zshrc
